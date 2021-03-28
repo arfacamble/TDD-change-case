4f88eaa53b5e0f714208829ba93456af9072e088
Arthur Campbell <arfa.camble@gmail.com>
2021-03-28 15:33:57 +0100

    tests passed

--- lib/change_case.rb
+++ lib/change_case.rb
@@ -3,3 +3,13 @@ def snake_to_camel(snake)
        .map.with_index { |word, index| index.zero? ? word : word.capitalize }
        .join
 end
+
+def camel_to_snake(camel)
+  snake_array = []
+  while (pos = /[A-Z]/ =~ camel)
+    snake_array << camel[0...pos]
+    camel = camel[pos].downcase + camel[pos+1..-1]
+  end
+  snake_array << camel
+  snake_array.join '_'
+end
