048c2736a41d22d1282b58fbbe9a3399c052de33
Arthur Campbell <arfa.camble@gmail.com>
2021-03-28 15:43:46 +0100

    final style changes to satisfy rubocop

--- lib/change_case.rb
+++ lib/change_case.rb
@@ -8,7 +8,7 @@ def camel_to_snake(camel)
   snake_array = []
   while (pos = /[A-Z]/ =~ camel)
     snake_array << camel[0...pos]
-    camel = camel[pos].downcase + camel[pos+1..-1]
+    camel = camel[pos].downcase + camel[pos + 1..-1]
   end
   snake_array << camel
   snake_array.join '_'
