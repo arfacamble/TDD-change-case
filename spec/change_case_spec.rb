1976bc7ffacc92afa37e2f0e9a1c3f34bb841bab
Arthur Campbell <arfa.camble@gmail.com>
2021-03-28 15:17:09 +0100

    further tests written

--- spec/change_case_spec.rb
+++ spec/change_case_spec.rb
@@ -6,10 +6,18 @@ snakes_and_camels = {
   'is_it_a_dromedary' => 'isItADromedary'
 }
 
-describe "snake_to_camel" do
+describe 'snake_to_camel' do
   snakes_and_camels.each do |snake, camel|
     it "should return #{camel} when given #{camel}" do
       expect(snake_to_camel(snake)).to eq camel
+    end
+  end
+end
+
+describe 'camel_to_snake' do
+  snakes_and_camels.each do |snake, camel|
+    it "should return #{snake} when given #{camel}" do
+      expect(camel_to_snake(camel)).to eq snake
     end
   end
 end
