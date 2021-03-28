1144ad4bbb564e3d88959517f7cff4de3449459e
Arthur Campbell <arfa.camble@gmail.com>
2021-03-28 15:04:54 +0100

    first test written

--- spec/change_case_spec.rb
+++ spec/change_case_spec.rb
@@ -1,7 +1,15 @@
 require 'change-case'
 
-snakes_to_camels = {
+snakes_and_camels = {
   'boa_constrictor' => 'boaConstrictor',
   'adder' => 'adder',
   'is_it_a_dromedary' => 'isItADromedary'
 }
+
+describe "snake_to_camel" do
+  snakes_and_camels.each do |snake, camel|
+    it "should return #{camel} when given #{camel}" do
+      expect(snake_to_camel(snake)).to eq camel
+    end
+  end
+end
