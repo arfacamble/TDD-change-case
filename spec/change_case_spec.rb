164dd70d232148c38d9d79c3870293948be8cade
Arthur Campbell <arfa.camble@gmail.com>
2021-03-29 17:57:15 +0100

    typo mistake corrected

--- spec/change_case_spec.rb
+++ spec/change_case_spec.rb
@@ -8,7 +8,7 @@ snakes_and_camels = {
 
 describe 'snake_to_camel' do
   snakes_and_camels.each do |snake, camel|
-    it "should return #{camel} when given #{camel}" do
+    it "should return #{camel} when given #{snake}" do
       expect(snake_to_camel(snake)).to eq camel
     end
   end
